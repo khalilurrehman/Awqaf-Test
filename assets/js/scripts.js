fe8ed6ac80b147b5679f84231352d486259e8c8d
Khalil Ur Rehman <khalil_rehman1993@ymail.com>
2022-04-03 18:11:45 +0400

    moving to live production
    live production

---
+++ assets/js/scripts.js
@@ -0,0 +1,146 @@
+/*===================================
+Author       : Khalil Ur Rehman.
+JS
+*===================================*/
+
+(function($) {
+	'use strict';
+	
+	/*===================================*
+	01. LOADING JS
+	/*===================================*/
+	$(window).on('load', function() {
+		var preLoder = $(".preloader");
+		preLoder.delay(700).fadeOut(500);
+	});
+
+	/*===================================*
+	02. SMOOTH SCROLLING JS
+	*===================================*/
+	// Select all links with hashes
+	var headerHeight = $(".header_wrap").height() - 10;
+    $('a.page-scroll').on('click', function(event) {
+        // On-page links
+        if ( location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname ) {
+          // Figure out element to scroll to
+          var target = $(this.hash),
+              speed= $(this).data("speed") || 800;
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top - headerHeight
+            }, speed);
+          }
+        }
+    });
+	
+	/*===================================*
+	03. MENU JS
+	*===================================*/
+	//Main navigation scroll spy for shadow
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
+
+	    if (scroll >= 50) {
+	        $('header').addClass('nav-fixed');
+	    } else {
+	        $('header').removeClass('nav-fixed');
+	    }
+
+	});
+	
+	
+	//Hide Navbar Dropdown After Click On Links
+	var navBar = $(".header_wrap");
+	var navbarLinks = navBar.find(".navbar-collapse ul li a.page-scroll");
+
+    $.each( navbarLinks, function( i, val ) {
+
+      var navbarLink = $(this);
+
+        navbarLink.on('click', function () {
+          navBar.find(".navbar-collapse").collapse('hide');
+		  $("header").removeClass("active");
+        });
+
+    });
+	
+	//Main navigation Active Class Add Remove
+	$('.navbar-toggler').on('click', function() {
+		$("header").toggleClass("active");
+		if($('.search-overlay').hasClass('open'))
+		{
+			$(".search-overlay").removeClass('open');
+			$(".search_trigger").removeClass('open');
+		}
+	});	
+	 
+	/*===================================*
+	04. SCROLLUP JS
+	*===================================*/
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 150) {
+			$('.scrollup').fadeIn();
+		} else {
+			$('.scrollup').fadeOut();
+		}
+	});
+	
+	$(".scrollup").on('click', function (e) {
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: 0
+		}, 600);
+		return false;
+	});
+	
+	/*===================================*
+	5. ANIMATION JS
+	*===================================*/
+	$(function() {
+		function ckScrollInit(items, trigger) {
+			items.each(function() {
+				var ckElement = $(this),
+					AnimationClass = ckElement.attr('data-animation'),
+					AnimationDelay = ckElement.attr('data-animation-delay');
+	
+				ckElement.css({
+					'-webkit-animation-delay': AnimationDelay,
+					'-moz-animation-delay': AnimationDelay,
+					'animation-delay': AnimationDelay,
+					opacity: 0
+				});
+	
+				var ckTrigger = (trigger) ? trigger : ckElement;
+	
+				ckTrigger.waypoint(function() {
+					ckElement.addClass("animated").css("opacity", "1");
+					ckElement.addClass('animated').addClass(AnimationClass);
+				}, {
+					triggerOnce: true,
+					offset: '90%',
+				});
+			});
+		}
+	
+		ckScrollInit($('.animation'));
+		ckScrollInit($('.staggered-animation'), $('.staggered-animation-wrap'));
+	
+	});
+	
+	/*===================================*
+	6. BACKGROUND IMAGE JS
+	*===================================*/
+	/*data image src*/
+	$(".background_bg").each(function() {
+		var attr = $(this).attr('data-img-src');
+		if (typeof attr !== typeof undefined && attr !== false) {
+			$(this).css('background', 'url(' + attr + ') center center/cover');
+		}
+	});
+	
+})(jQuery);
